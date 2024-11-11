a36de01a2a0065bcf49df1877d3b6914d61a8253
Ibraheem014 <66434732+Ibraheem014@users.noreply.github.com>
2024-11-11 18:13:04 -0500

    initial commit

---
+++ model.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+
+class Unet(nn.module):
+    def __init__():
+        pass
+
+    def forward():
+        pass
+
+class UNetBlock(nn.module):
+    """
+    Defines one layer in the U-Net architecture 
+    """
+    def __init__():
+        pass
+
+    def forward():
+        pass
+
+class Discriminator(nn.Module):
+    """
+    Implements a PatchGAN discriminator
+    """
+    def __init__():
+        pass
+
+    def forward():
+        pass
